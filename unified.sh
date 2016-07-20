4960605a27d2cfe21979d70f171851dfbb2878e3
Jared Szechy <jared.szechy@brilligent.com>
2016-07-20 11:57:28 -0400

    add more ignores

--- unified.sh
+++ unified.sh
@@ -6,5 +6,6 @@ do
     stow --no-folding -t /usr/local/unified \
         --ignore=CHANGES --ignore=INSTALL_RECEIPT.json \
         --ignore=ChangeLog --ignore=AUTHORS \
+        --ignore=ChangeLog.md --ignore=TODO \
         -d /usr/local/Cellar/$name $version
 done
