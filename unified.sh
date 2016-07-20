75c8f18f088fa6f0bb2cdb633dcb05ac239c4736
Jared Szechy <jared.szechy@brilligent.com>
2016-07-20 10:51:44 -0400

    ignore more files

--- unified.sh
+++ unified.sh
@@ -5,5 +5,6 @@ do
     version=${dep#*:}
     stow --no-folding -t /usr/local/unified \
         --ignore=CHANGES --ignore=INSTALL_RECEIPT.json \
+        --ignore=ChangeLog --ignore=AUTHORS \
         -d /usr/local/Cellar/$name $version
 done
