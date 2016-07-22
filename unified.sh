0f72586b292a4150cba33888b0a476c04852027f
Jared Szechy <jared.szechy@gmail.com>
2016-07-21 22:10:40 -0400

    Create symlink for OpenGL headers

--- unified.sh
+++ unified.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
-mkdir -p /usr/local/unified
+
+UNIFIED_DIR=/usr/local/unified
+CELLAR_DIR=/usr/local/Cellar
+XCODE_APP_GL=$(xcrun --show-sdk-path)/System/Library/Frameworks/OpenGL.framework/Headers
+XCODE_CMD_GL=/System/Library/Frameworks/OpenGL.framework/Headers
+
+mkdir -p $UNIFIED_DIR/include
+
+if [ -d "$XCODE_CMD_GL" ]; then
+    ln -sfn $XCODE_CMD_GL $UNIFIED_DIR/include/GL
+elif [ -d "$XCODE_APP_GL" ]; then
+    ln -sfn $XCODE_APP_GL $UNIFIED_DIR/include/GL
+else
+    echo "Could not locate OpenGL headers. Is Xcode installed?"
+fi
+
+# Symlink all brew dependencies into single directory
 for dep in "$@"
 do
     name=${dep%:*}
     version=${dep#*:}
-    stow --no-folding -t /usr/local/unified \
+    stow --no-folding -t $UNIFIED_DIR \
         --ignore=CHANGES --ignore=INSTALL_RECEIPT.json \
         --ignore=ChangeLog --ignore=AUTHORS \
         --ignore=ChangeLog.md --ignore=TODO \
         --ignore=NEWS \
-        -d /usr/local/Cellar/$name $version
+        -d $CELLAR_DIR/$name $version
 done
